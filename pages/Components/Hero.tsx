683f5559febd3bb1f76fb872201a38bdc9a715e9
Ansh Narula <2019210@iiitdmj.ac.in>
2022-02-09 02:16:18 +0530

    added bootstrap and hero component

---
+++ pages/Components/Hero.tsx
@@ -0,0 +1,15 @@
+const Hero = () => {
+  return (
+    <>
+      <h1 className="name display-2">
+        Hello there, <br />I am Ansh Narula
+      </h1>
+      <div className="my-5">
+        I am a passionate full stack developer with background knowledge of MERN
+        stack with redux.
+      </div>
+    </>
+  );
+};
+
+export default Hero;
