35c0c126e5a812dd82fd92f980e0aa8750270b9e
Ansh Narula <2019210@iiitdmj.ac.in>
2022-02-15 22:56:50 +0530

    colors

--- pages/Components/TechCard.tsx
+++ pages/Components/TechCard.tsx
@@ -1,24 +1,22 @@
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
-import react from "/img/react.svg";
+function TechCard({ img, name }: { img: any; name: any }) {
 
-function TechCard({ img, name }: { img: any, name : any }) {
-  useEffect(() => {
-    console.log(img.src)
-  }, [] )
   return (
-    <div className="techCard m-1 d-flex flex-column justify-content-between">
+    <div
+      className="techCard m-1 d-flex flex-column justify-content-between"
+    >
       <Image
         src={img.src}
-        width= "1rem"
-        height= "1rem"
+        width="1rem"
+        height="1rem"
         alt=""
         layout="responsive"
       />
       <span className="text-center mt-2">{name}</span>
     </div>
   );
-};
+}
 
 export default TechCard;
