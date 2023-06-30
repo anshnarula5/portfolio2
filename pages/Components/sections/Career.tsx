2dcabeb556f5ef39fa1fe8516d58a07b10801b92
Ansh Narula <2019210@iiitdmj.ac.in>
2023-06-30 16:06:20 +0530

    added gtag for google analystics

--- pages/Components/sections/Career.tsx
+++ pages/Components/sections/Career.tsx
@@ -4,6 +4,17 @@ const Career = () => {
             <div className="careerCard py-3 px-4 mb-3">
                 <div className="d-flex justify-content-between align-items-center">
                     <p>SDE Intern</p>
+                    <span className="blockquote-footer">Jan 2023 - Present</span>
+                </div>
+                <h2>Amazon</h2>
+                <p className="secondaryText">
+                    Worked in Alexa multimodal team in smart home domain. Developed an integration solution for a smart home experience service, ensuring compliance with privacy regulations. I built an event-driven infrastructure using AWS services, implemented deletion request handling, and established secure connections between services,
+                    demonstrating strong problem-solving skills and collaborative teamwork.
+                </p>
+            </div>
+            <div className="careerCard py-3 px-4 mb-3">
+                <div className="d-flex justify-content-between align-items-center">
+                    <p>SDE Intern</p>
                     <span className="blockquote-footer">May 2022 - Aug 2022</span>
                 </div>
                 <h2>Biobazaar Healthtech</h2>
@@ -12,17 +23,6 @@ const Career = () => {
                 </p>
                 <p className="secondaryText">
                     Tech stack used : React, Node, Typescript, Postgres, Mocha.
-                </p>
-
-            </div>
-            <div className="careerCard py-3 px-4 mb-3">
-                <div className="d-flex justify-content-between align-items-center">
-                    <p>SDE Intern</p>
-                    <span className="blockquote-footer">Jan 2023 - Present</span>
-                </div>
-                <h2>Amazon</h2>
-                <p className="secondaryText">
-                    Will work with Alexa multmodal initiative team.
                 </p>
             </div>
         </div>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import Script from "next/script";
+import * as gtag from "../gtag"
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -38,6 +40,24 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
+         <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       </Head>
       <Component {...pageProps} />
     </>
